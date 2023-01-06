e2ffa502700f9219b47de8421fe612608b262131
iancenry <iancenry@gmail.com>
2023-01-07 00:13:17 +0300

    initial submission for the project

---
+++ README.md
@@ -0,0 +1,2 @@
+#Password and Email Validation
+This is a small project showing how to validate emails and passwords on a login page. The icons used are bootstrap icons. 
---
+++ index.js
@@ -0,0 +1,10 @@
+const form  = document.querySelector("form"),
+    emailField  = form.querySelector(".email-field"),
+    emailInput  = emailField.querySelector(".email"),
+    passwordField  = form.querySelector(".create-password"),
+    passwordInput  = passwordField.querySelector(".password"),
+    confirmPasswordField  = form.querySelector(".confirm-password"),
+    confirmPasswordInput  = form.querySelector(".confirmPassword");
+
+
+    
